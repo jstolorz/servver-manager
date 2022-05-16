39561e014916964dcd98d29a80e7146e2f9b931e
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-05-16 15:29:01 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/servvermanager/ServverManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.servvermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServverManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServverManagerApplication.class, args);
+    }
+
+}
